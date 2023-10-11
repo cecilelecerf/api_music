2f818556f28d45618b39eeeb2958a1a639549c0e
Cécile Lecerf <cecel@Cecoule-Macbook.local>
2023-10-11 15:29:50 +0200

    create a server and add mangoose

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const mongoose = require ("mongoose");
+
+//avec une install local
+mongoose.connect('mongodb://mongo/apinode')
+
+//avec docker (mongodb://nomDuContainer/NomdeLaBDD)
+// mongoose.connect('mongodb://mongo/apinode')
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
